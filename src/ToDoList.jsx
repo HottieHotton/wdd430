4d42533d68d4d6404df39cfc28c2e431c629cb9c
Braxton Hotton <bhotton25@gmail.com>
2024-10-26 22:13:38 -0600

    Completed W01 To-Do Activity

---
+++ src/ToDoList.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { ToDoItem } from "./ToDoItem";
+
+export function ToDoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No To-Do's!"}
+      {todos.map((todo) => {
+        return <ToDoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>;
+      })}
+    </ul>
+  );
+}
